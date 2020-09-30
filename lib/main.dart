071f4a713d6e55cfa1d747ddb5d14bd577bd59a3
Prashant Singh <prashantsingh061201@gmail.com>
2020-10-01 00:18:32 +0530

    Update main.dart
    changes..

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,9 @@ class MyApp extends StatelessWidget {
       //   primaryColor: Color(0xff075e54),
       //   accentColor: Color(0xff45CE30)
       // ),
-      // title: "Whatsapp clone",
+      // title: "Whatsapp UI Clone",
       // home: HomePage(),
+      // 
       // debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     );
